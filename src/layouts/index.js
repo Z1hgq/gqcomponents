d3b0fdea55d0b868a04dc4990e7ed4ef0033e124
Z1hgq <z11hgq@gmail.com>
2020-02-07 16:06:04 +0800

    【BUG】container auto hight bugs

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,8 +15,16 @@ export default function Layout(props) {
   if (pageItem) {
     return children;
   }
+  useEffect(() => {
+    const content = document.getElementById("content");
+    content.style.height = document.body.clientHeight - 60 + "px";
+    window.onresize = function () {
+      console.log(document.body.clientHeight);
+      content.style.height = document.body.clientHeight - 60 + "px";
+    };
+  }, []);
   return (
-    <div className="ceiba3-layout">
+    <div className="gq-layout">
       <div id="header">
         <div className="img-box">
           <img
@@ -27,7 +35,7 @@ export default function Layout(props) {
         <span className="title-sub">{appName}</span>
         <Header />
       </div>
-      <div className="ceiba3-layout-content">
+      <div className="gq-layout-content" id="content">
         <div className="nav">
           <Nav />
         </div>
